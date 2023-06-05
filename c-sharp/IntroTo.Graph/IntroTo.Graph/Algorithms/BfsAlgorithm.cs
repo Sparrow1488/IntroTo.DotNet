bac2d25314d4c3d8be6c477f19de214fb08dac96
Sparrow1488 <ilyaokubev@gmail.com>
2023-06-05 13:47:09 +0300

    feat: bfs save path

--- c-sharp/IntroTo.Graph/IntroTo.Graph/Algorithms/BfsAlgorithm.cs
+++ c-sharp/IntroTo.Graph/IntroTo.Graph/Algorithms/BfsAlgorithm.cs
@@ -2,6 +2,63 @@ namespace IntroTo.Graph.Algorithms;
 
 public static class BfsAlgorithm
 {
+    public static List<Vertex> Bfs(Graph graph, Vertex start, Vertex finish)
+    {
+        var visited = new HashSet<Vertex>(); // black
+        var calculated = new Dictionary<Vertex, int>(); // gray
+        var history = new Dictionary<Vertex, List<Vertex>>();
+        
+        var queue = new Queue<Vertex>();
+        queue.Enqueue(start);
+
+        visited.Add(start);
+        calculated[start] = 0;
+
+        while (queue.Count > 0)
+        {
+            var current = queue.Dequeue();
+            foreach (var adjacent in graph.GetAdjacentVertices(current))
+            {
+                if (!history.TryAdd(adjacent, new List<Vertex> { current }))
+                {
+                    var previous = history[adjacent];
+                    previous.Add(current);
+                }
+                
+                if (!visited.Contains(adjacent) && !calculated.ContainsKey(adjacent))
+                {
+                    calculated.Add(adjacent, calculated[current] + 1);
+                    visited.Add(adjacent);
+                    queue.Enqueue(adjacent);
+
+                    if (adjacent == finish)
+                    {
+                        return GetPath(start, finish, history);
+                    }
+                }
+            }
+        }
+
+        return new List<Vertex>();
+    }
+
+    private static List<Vertex> GetPath(Vertex start, Vertex finish, Dictionary<Vertex, List<Vertex>> history)
+    {
+        var path = new List<Vertex> { finish };
+        var current = finish;
+        
+        while (current != start)
+        {
+            var visitedList = history[current];
+            var previous = visitedList.First();
+            current = previous;
+            path.Add(current);
+        }
+
+        path.Reverse();
+        return path;
+    }
+    
     public static void Bfs(Graph graph, Vertex start)
     {
         var visited = new HashSet<Vertex>(); // black
--- c-sharp/IntroTo.Graph/IntroTo.Graph/Program.cs
+++ c-sharp/IntroTo.Graph/IntroTo.Graph/Program.cs
@@ -1,8 +1,14 @@
-﻿using IntroTo.Graph.Helpers;
+﻿using IntroTo.Graph.Algorithms;
+using IntroTo.Graph.Helpers;
 
 Console.WriteLine("Hello, Graph!");
 
-var graph = GraphGenerator.GenerateCellField(10);
+var graph = GraphGenerator.GenerateCellField(3);
 
+BfsAlgorithm.Bfs(graph, graph.Vertices.First(x => x.Id == 9));
+var route = BfsAlgorithm.Bfs(
+    graph, 
+    graph.Vertices.First(x => x.Id == 1), 
+    graph.Vertices.First(x => x.Id == 8)
+);
 GraphViewer.ShowVerticesList(graph);
-// GraphViewer.ShowMatrix(graph);
