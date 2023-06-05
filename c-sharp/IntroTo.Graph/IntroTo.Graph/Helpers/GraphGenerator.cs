505ed2266feb2d5ce37f2e3d0660c03e3984cdc7
Sparrow1488 <ilyaokubev@gmail.com>
2023-06-05 12:12:56 +0300

    feat: cell graph generator

---
+++ c-sharp/IntroTo.Graph/IntroTo.Graph/Helpers/GraphGenerator.cs
@@ -0,0 +1,46 @@
+namespace IntroTo.Graph.Helpers;
+
+public static class GraphGenerator
+{
+    public static Graph GenerateCellField(int size)
+    {
+        var field = new Vertex[size, size];
+        var edges = new List<Edge>();
+
+        field[0, 0] = new Vertex(1);
+        
+        var counter = 2;
+        for (var rowI = 0; rowI < size; rowI++)
+        {
+            for (var columnI = 0; columnI < size; columnI++)
+            {
+                var current = field[rowI, columnI];
+
+                if (columnI + 1 < size) // next
+                {
+                    field[rowI, columnI + 1] ??= new Vertex(counter);
+                    edges.Add(new Edge(current, field[rowI, columnI + 1], 1));
+                }
+
+                if (rowI + 1 < size) // bottom
+                {
+                    field[rowI + 1, columnI] ??= new Vertex(counter + size - 1);
+                    edges.Add(new Edge(current, field[rowI + 1, columnI], 1));
+                }
+
+                counter++;
+            }
+        }
+
+        var verticesList = new List<Vertex>(size * size);
+        for (var i = 0; i < size; i++)
+        {
+            for (var j = 0; j < size; j++)
+            {
+                var vertex = field[i, j];
+                verticesList.Add(vertex);
+            }
+        }
+        return new Graph(edges, verticesList);
+    }
+}
--- c-sharp/IntroTo.Graph/IntroTo.Graph/Program.cs
+++ c-sharp/IntroTo.Graph/IntroTo.Graph/Program.cs
@@ -1,27 +1,27 @@
-﻿using IntroTo.Graph;
-using IntroTo.Graph.Algorithms;
-using IntroTo.Graph.Helpers;
+﻿using IntroTo.Graph.Helpers;
 
 Console.WriteLine("Hello, Graph!");
 
-var vertices = Enumerable.Range(1, 9)
-    .Select(x => new Vertex(x))
-    .ToDictionary(x => x.Id);
+var graph = GraphGenerator.GenerateCellField(5);
 
-var edges = new List<Edge>
-{
-    new(vertices[1], vertices[2], 3),
-    new(vertices[2], vertices[3], 9),
-    new(vertices[2], vertices[4], 4),
-    new(vertices[3], vertices[4], 1),
-    new(vertices[1], vertices[5], 5),
-    new(vertices[5], vertices[6], 7),
-    new(vertices[6], vertices[8], 3),
-    new(vertices[6], vertices[9], 3),
-    new(vertices[4], vertices[8], 3)
-};
-
-var graph = new Graph(edges, vertices.Select(x => x.Value).ToList());
+// var vertices = Enumerable.Range(1, 9)
+//     .Select(x => new Vertex(x))
+//     .ToDictionary(x => x.Id);
+//
+// var edges = new List<Edge>
+// {
+//     new(vertices[1], vertices[2], 3),
+//     new(vertices[2], vertices[3], 9),
+//     new(vertices[2], vertices[4], 4),
+//     new(vertices[3], vertices[4], 1),
+//     new(vertices[1], vertices[5], 5),
+//     new(vertices[5], vertices[6], 7),
+//     new(vertices[6], vertices[8], 3),
+//     new(vertices[6], vertices[9], 3),
+//     new(vertices[4], vertices[8], 3)
+// };
+//
+// var graph = new Graph(edges, vertices.Select(x => x.Value).ToList());
 
 GraphViewer.ShowVerticesList(graph);
 GraphViewer.ShowMatrix(graph);
